4ad76e75a25e15fd5db106ee96c524ec8bc4b356
Eiekl <dakor82@inbox.ru>
2022-07-31 18:08:00 +0300

    1

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,127 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+    private Connection connection;
+    {
+        try {
+            connection = Util.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public UserDaoJDBCImpl() throws SQLException {
 
     }
 
     public void createUsersTable() {
-
+        String createTable = "create table  if not exists users (id serial," +
+                "name varchar(20)," +
+                "lastName varchar(30), " +
+                "age smallint, " +
+                "PRIMARY KEY (id))";
+        try (Statement statement = connection.createStatement();) {
+            connection.setAutoCommit(false);
+            statement.executeUpdate(createTable);
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
+        String dropTable = "drop table if exists users";
 
+        try (Statement statement = connection.createStatement();) {
+            connection.setAutoCommit(false);
+            statement.executeUpdate(dropTable);
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String saveUser = "insert into users (name, lastname, age) values (?,?,?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(saveUser)) {
+            connection.setAutoCommit(false);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+            System.out.printf("Пользователь %s добавлен в базу\n", name);
+            connection.commit();
 
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        String deleteUser = "delete from users where id = ?";
+        try (PreparedStatement preparedStatement =
+                     connection.prepareStatement(deleteUser)) {
+            connection.setAutoCommit(false);
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        String showALL = "select * from users";
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(showALL);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong(1));
+                user.setName(resultSet.getString(2));
+                user.setLastName(resultSet.getString(3));
+                user.setAge(resultSet.getByte(4));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
+        String deleteALL = "delete from users";
+        try (Statement statement = connection.createStatement()) {
+            connection.setAutoCommit(false);
+            statement.executeUpdate(deleteALL);
+            connection.commit();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        }
 
     }
 }
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -59,4 +59,9 @@ public class User {
     public void setAge(Byte age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return String.format("name: %s, lastName: %s, age: %s./n", getName(), getLastName(), getAge());
+    }
 }
